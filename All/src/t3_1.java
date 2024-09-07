c6cddbb1cb80edb9baeeaff5808215627edac296
nikita <professornikson@gmail.com>
2024-09-07 21:52:26 +0300

    Задание 3.1 Консольный ввод – вывод

---
+++ All/src/t3_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class t3_1 {
+    static public void main(String[] args){
+        System.out.print("Введите Ваше имя: ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+    }
+}
