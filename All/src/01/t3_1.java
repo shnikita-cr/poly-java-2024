8e16300943dea377c652027a65d9454c24cb5225
nikita <professornikson@gmail.com>
2024-09-07 22:50:01 +0300

    Распределение файлов по папкам модулей

--- All/src/01/t3_1.java
+++ All/src/01/t3_1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class t3_1 {
-    static public void main(String[] args){
+    static public void main(String[] args) {
         System.out.print("Введите Ваше имя: ");
         Scanner scan = new Scanner(System.in);
         String name = scan.next();
