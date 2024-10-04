1ae677c403f7fef3b57a4749ff7996a8e4c95737
nikita <professornikson@gmail.com>
2024-10-04 23:41:31 +0300

    Обновлено задание 01_03

--- All/src/ch_01/sect_03/task_01_03.java
+++ All/src/ch_01/sect_03/task_01_03.java
@@ -1,6 +1,8 @@
+package ch_01.sect_03;
+
 import java.util.Scanner;
 
-public class t01_3_1 {
+public class task_01_03 {
     static public void main(String[] args) {
         System.out.print("Введите Ваше имя: ");
         Scanner scan = new Scanner(System.in);
