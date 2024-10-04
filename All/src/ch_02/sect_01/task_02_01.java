9ae7b0e8c536b70b350de786c53d093c97784fa6
nikita <professornikson@gmail.com>
2024-10-05 00:03:08 +0300

    Обновлено задание 02_01

--- All/src/ch_02/sect_01/task_02_01.java
+++ All/src/ch_02/sect_01/task_02_01.java
@@ -1,6 +1,6 @@
-// названия классов содержат нумерацию практик в лекции
+package ch_02.sect_01;
 
-class t02_1_1 {//практика #1
+class task_02_01_1 {//практика #1
 
     static public void main(String[] args) {
         byte b0 = 0, b1 = -128, b2 = 127;
@@ -16,36 +16,41 @@ class t02_1_1 {//практика #1
     }
 }
 
-class t02_1_2 {
+class task_02_01_2 {//практика #2
+
     static public void main(String[] args) {
         String name = "\u041d\u0438\u043a\u0438\u0442\u0430!";
         System.out.println("Имя моё " + name);
     }
 }
 
-class t02_1_3 {
+class task_02_01_3 {//практика #3
+
     static public void main(String[] args) {
         int a = 1;
         {
             int b = 2;
         }
-//        int c = a + b; // в переменной с не будет значения, так как она зависит от b,
-        // время жизни которой закончилось после скобки }, код
-        // не скомпилируется
+        // int c = a + b; // в переменной с не будет значения, так как она зависит от b,
+        // время жизни которой закончилось после скобки },
+        // код не скомпилируется
     }
 }
 
-class t02_1_4 {
+class task_02_01_4 {//практика #4
+
     static public void main(String[] args) {
         String name = "Иван", surname = "Иванов";
         String fullName = name + ' ' + surname;
         // name = String - объект
         // ' '  = Char - базовый тип
+        String s1 = "a" + surname;
         System.out.println(fullName);
     }
 }
 
-class t02_1_5 {
+class task_02_01_5 {//практика #5
+
     static public void main(String[] args) {
         float pi = 3.14f;
         float pi2 = pi * 2; // умножение float int -> float, так как
@@ -58,33 +63,37 @@ class t02_1_5 {
     }
 }
 
-class t02_1_6 {
+class task_02_01_6 {//практика #6
+
     static public void main(String[] args) {
         int i = 95;
         float pi = 3.14f;
 
-        short si = (short) i;
-        byte bi = (byte) (i % 10);
-        float di = (float) (3.5 * pi);
+        short si = (short) i;  // явное преобразование к short
+        byte bi = (byte) (i % 10); // явное преобразование к byte
+        float di = (float) (3.5 * pi); // явное преобразование к float
     }
 }
 
-class t02_1_7 {
+class task_02_01_7 {//практика #7
+
     static public void main(String[] args) {
         int a = 120;
-        byte b = a + 10;
-//         ошибка: переменная меньшего типа инициализируется выражением широкого типа без явного сужения
-        byte c = (byte) (a + 10);
-//         ок, значение 130, выполнено явное сужающее преобразование к byte
-        byte d = a + 1;
+        // byte b = a + 10;
         // ошибка: переменная меньшего типа инициализируется выражением широкого типа без явного сужения
+        byte c = (byte) (a + 10);
+        // ок, значение -126, выполнено явное сужающее преобразование к byte
+        // byte d = a + 1;
+        // ошибка компиляции: переменная меньшего типа инициализируется выражением широкого типа без явного сужения
     }
 }
 
-class t02_1_8 {
+class task_02_01_8 {//практика #8
+
     static public void main(String[] args) {
         int a = 5;
         var c = (byte) a + 4; // var разворачивается в byte из-за явного преобразования
-        var k = 5/6.5; // var разворачивается в double из-за результата деления int / double -> double
+        var k = 5 / 6.5; // var разворачивается в double из-за результата деления int / double -> double
     }
 }
+
