0ce6ee9025c1c48dc1abdafbe556774d74564b64
nikita <professornikson@gmail.com>
2024-10-21 23:40:12 +0300

    Добавлено задание 02_05

---
+++ All/src/ch_02/sect_05/task_02_05_01.java
@@ -0,0 +1,17 @@
+package ch_02.sect_05;
+
+public class task_02_05_01 {
+    public static void main(String[] args) {
+        int a = 3;
+        int i = 0;
+        if (a == 1) { // standart if
+            i = 1;
+        } else if (a == 2) {
+            i = 2;
+        } else {
+            i = 3;
+        }
+        
+        i = a == 1 ? 1 : (a == 2 ? 2 : 3); // ?: construction
+    }
+}
