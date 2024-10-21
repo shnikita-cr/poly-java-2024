19f687ddd9a63d0bbc6eb4836e73a3c499d10188
nikita <professornikson@gmail.com>
2024-10-21 23:10:33 +0300

    Добавлено задание 02_07

---
+++ All/src/ch_02/sect_07/task_02_07_01.java
@@ -0,0 +1,15 @@
+package ch_02.sect_07;
+
+public class task_02_07_01 {
+    public void f1() {
+        while (true) {
+            System.out.println("while");
+        }
+    }
+
+    public void f2() {
+        do {
+            System.out.println("do-while");
+        } while (true);
+    }
+}
---
+++ All/src/ch_02/sect_07/task_02_07_02.java
@@ -0,0 +1,17 @@
+package ch_02.sect_07;
+
+public class task_02_07_02 {
+    public static void main(String[] args) {
+        int k = 0;
+        for (int i = 1; i <= 10; i++) {
+            k += i;
+            if (k % i == 0) {
+                continue;
+            }
+            k += 1;
+            if (k == 32)
+                break;
+        }
+    }
+}
+
