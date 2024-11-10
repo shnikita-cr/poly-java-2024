b79b723c3ea57d9d469c569064a0f696625ed681
nikita <professornikson@gmail.com>
2024-11-10 21:56:32 +0300

    Добавлено задание 03_11

---
+++ All/src/ch_03/sect_11/task_03_11_01.java
@@ -0,0 +1,27 @@
+package ch_03.sect_11;
+
+public class task_03_11_01 {
+}
+
+class A1 {
+    public final int a;
+
+    public A1() {
+        this.a = 1;
+    }
+}
+
+class A2 {
+    public final int a = 1;
+}
+
+
+class A3 {
+    public final int a;
+
+    {
+        a = 1;
+    }
+}
+
+
