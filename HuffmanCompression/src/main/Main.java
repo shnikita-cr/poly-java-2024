4744b984a60b59cb34aa876f698b756cff4aa0d6
nikita <professornikson@gmail.com>
2024-11-22 02:05:41 +0300

    Добавлена начальная версия программы

---
+++ HuffmanCompression/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Huffman Compression");
+        File file
+        HuffmanCompressor compressor;
+    }
+}
