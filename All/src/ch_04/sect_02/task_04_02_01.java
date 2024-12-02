4afc50d327b792de4d61e7995bb6a59a9fc558d7
nikita <professornikson@gmail.com>
2024-12-02 23:33:24 +0300

    Добавлено 04_02

---
+++ All/src/ch_04/sect_02/task_04_02_01.java
@@ -0,0 +1,9 @@
+package ch_04.sect_02;
+
+public class task_04_02_01 {
+    final void m(){}
+}
+
+class task_04_02_01_ extends task_04_02_01 {
+//    void m() {} // ошибка - метод в суперклассе обявлен как final
+}
---
+++ All/src/ch_04/sect_02/task_04_02_02.java
@@ -0,0 +1,6 @@
+package ch_04.sect_02;
+
+final public class task_04_02_02 {
+}
+
+//class task_04_02_02_ extends task_04_02_02 {} // ошибка - суперкласс обявлен как final
