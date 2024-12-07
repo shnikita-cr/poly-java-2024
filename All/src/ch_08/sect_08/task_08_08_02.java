16be2a71f9d640bf0de8d33188f6907d1024f253
nikita <professornikson@gmail.com>
2024-12-07 14:17:15 +0300

    Обновлено 08_08 (Практика 2)

--- All/src/ch_08/sect_08/task_08_08_02.java
+++ All/src/ch_08/sect_08/task_08_08_02.java
@@ -1,9 +1,22 @@
 package ch_08.sect_08;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+
 public class task_08_08_02 {
     public static void main(String[] args) {
-        //TODO
-        // Практика #2
-        // Зачем нужен метод flush()?
+        /*
+        метод flush() в Formatter используется, чтобы принудительно записать все данные,
+        накопленные в буфере, в целевой поток (например, файл)
+        таким образом файлы могут быть сразу записаны, а не будут ждать автоматической чистки буфера
+        */
+        try (Formatter formatter = new Formatter(new FileWriter("output.txt"))) {
+            formatter.format("Hello, %s!", "World");
+            formatter.flush(); // принудительно записываем накопленные данные
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        // без вызова flush() данные могут остаться в буфере до закрытия Formatter или заполнения буфера
     }
 }
