5bd5559666f16c34f0e5cfcab45800386fe846ce
nikita <professornikson@gmail.com>
2024-12-07 14:08:51 +0300

    Обновлено 09_03

--- All/src/ch_09/sect_03/task_09_03_01.java
+++ All/src/ch_09/sect_03/task_09_03_01.java
@@ -1,32 +1,60 @@
 package ch_09.sect_03;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Scanner;
+class Ex1 extends Exception {
+    public Ex1(String message) {
+        super(message);
+    }
+}
+
+class Ex2 extends Ex1 {
+    public Ex2(String message) {
+        super(message);
+    }
+}
+
+class Ex3 extends Ex2 {
+    public Ex3(String message) {
+        super(message);
+    }
+}
 
 public class task_09_03_01 {
     public static void main(String[] args) {
-        /*
-        Приведите пример кода, в котором продемонстрированы следующие ситуации:
+        f1(1);
+        f2(3);
+    }
 
-    несколько исключений обрабатываются абсолютно идентичным образом;
-    исключения, которые обрабатываются, образуют иерархию Ex1 <|-- Ex2 <|-- Ex3, т.е. Ex3 - подкласс Ex2, а Ex2 - подкласс Ex1.
-
-         */
-        String filename;
-        Scanner sc = new Scanner(System.in);
-        filename = sc.nextLine();
-
-        System.out.println("Print text to save into file: ");
-        try (FileOutputStream f = new FileOutputStream(filename)) {
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                f.write(line.getBytes());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    private static void f1(Integer a) {
+        try {
+            processArgument(a);
+        } catch (Ex1 | IllegalArgumentException e) { // Идентичная обработка
+            System.out.println("Exception handled: " + e.getMessage());
         }
+    }
+
+    public static void f2(Integer a) {
+        try {
+            processArgument(a);
+        } catch (Ex3 ex3) {
+            System.out.println("Ex3 handled: " + ex3.getMessage());
+        } catch (Ex2 ex2) {
+            System.out.println("Ex2 handled: " + ex2.getMessage());
+        } catch (Ex1 ex1) {
+            System.out.println("Ex1 handled: " + ex1.getMessage());
+        } catch (Exception ex) {
+            System.out.println("General Exception handled: " + ex.getMessage());
+        }
+    }
+
+    private static void processArgument(Integer a) throws Ex1 {
+        if (a == 1)
+            throw new Ex1("trivial a");
+        if (a % 2 == 1)
+            throw new Ex2("odd a ");
+        if (a < 0)
+            throw new Ex3("negative a");
+        if (a == Integer.MAX_VALUE)
+            throw new IllegalArgumentException();
+        System.out.println(a);
     }
 }
--- All/src/ch_09/sect_03/task_09_03_02.java
+++ All/src/ch_09/sect_03/task_09_03_02.java
@@ -1,4 +1,13 @@
 package ch_09.sect_03;
 
 public class task_09_03_02 {
+    public static void main(String[] args) {
+        // объявление final в catch делает переменную исключения неизменяемой
+        try {
+            throw new Exception("Test exception");
+        } catch (final Exception e) {
+            // e = new Exception("New exception"); // Ошибка: нельзя переназначить e
+            System.out.println(e.getMessage());
+        }
+    }
 }
